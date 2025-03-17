bf67b7b19942684a287243d7dee5b69c496800eb
alireza <areza.mirzayi@gmail.com>
2025-03-17 11:47:06 +0330

    add register page

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,36 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserChangeForm
+from django import forms
 
 from users.models import CustomUser
 
 
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = CustomUser
-        fields = ('email', 'full_name')
+class CustomUserCreationForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Enter You Email Address'}),
+        label='Email',
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Password'}),
+        label='Password'
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}),
+        label='Confirm Password'
+    )
+
+    def clean_re_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('re_password')
+        if confirm_password != password:
+            raise forms.ValidationError('passwords do not match')
+        return password
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        is_exist_user_email = CustomUser.objects.filter(email=email).exists()
+        if is_exist_user_email:
+            raise forms.ValidationError('this email already exists')
+        return email
 
 
 class CustomUserChangeForm(UserChangeForm):
--- users/views.py
+++ users/views.py
@@ -2,13 +2,16 @@ from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect
 
 from users.forms import CustomUserCreationForm
+from users.models import CustomUser
 
 
 def register_view(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            user = CustomUser.objects.create_user(email=email, password=password)
             login(request, user)
             return redirect("home")
     else:
