a00fe6105f9e7096d6b7a74c8c9c83e3291ac581
alireza <areza.mirzayi@gmail.com>
2025-04-18 17:07:09 +0330

    add todo model, app, list view

--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+@login_required(login_url="/users/login/")
 def list_todos_view(request):
     return render(request, "list_todos.html")
