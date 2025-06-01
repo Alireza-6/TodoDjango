de3b702b94e9e0c94860e09ff01831428ce60601
alireza <areza.mirzayi@gmail.com>
2025-06-01 18:36:23 +0330

    complete list todos page

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from todos.models import Todo
+
 
 @login_required(login_url="/users/login/")
 def list_todos_view(request):
-    return render(request, "list_todos.html")
+    uncompleted_todos = Todo.objects.filter(is_completed=False)
+    completed_todos = Todo.objects.filter(is_completed=True)
+    return render(
+        request, "list_todos.html", {"uncompleted_todos": uncompleted_todos, "completed_todos": completed_todos}
+    )
